43fc47c4c111d3656ccd2648fa79d279d6e9bc58
alex_Palladis <alehandro4443@gmail.com>
2024-12-06 14:32:21 +0200

    complete sign up page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,29 +3,29 @@ import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to='/'>
+    <header className="bg-gradient-to-r from-sky-500 via-purple-500 to-indigo-500 shadow-md">
+        <div className="container mx-auto flex justify-between items-center max-w-6xl p-4">
+            <Link to='/' className="flex items-center space-x-2">
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-400'>RealEstate</span>
-                    <span className='text-slate-600'>App</span>
+                    <span className="text-2xl font-bold text-white">🏠RealEstate</span>
+                    <span className='text-2xl font-semibold text-slate-700'>App</span>
                 </h1>
             </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <FaSearch className='text-slate-600'/>
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center shadow-md px-4 py-2'>
+                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none text-gray-600 w-24 sm:w-64'/>
+                <FaSearch className='text-gray-400'/>
             </form>
             <ul className='flex gap-4 '>
                 <Link to='/'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
+                    <li className='hidden sm:inline text-sm text-white font-medium cursor-pointer hover:text-gray-400 transition'>Home</li>
                 </Link>
 
                 <Link to='/about'>
-                    <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
+                    <li className='hidden sm:inline text-sm text-white font-medium cursor-pointer hover:text-gray-400 transition'>About</li>
                 </Link>
                 
                 <Link to='/sign-in'>
-                    <li className=' text-slate-700 hover:underline cursor-pointer'>Sign In</li>
+                    <li className='text-white text-sm font-medium bg-indigo-600 px-4 py-2 rounded-lg hover:bg-indigo-500 transition cursor-pointer'>Sign In</li>
                 </Link>
             </ul>
         </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg' id='username'/>
+        <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email'/>
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg' id='password'/>
+        <button className='text-white bg-blue-500 p-3 rounded-lg uppercase hover:opacity-70 disabled:opacity-50'>Sign Up</button>
+      </form>
+      <div className='flex gap-2 mt-10'>
+        <p>Already have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Sign In</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
