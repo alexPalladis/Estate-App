edfb44b201105a6990947f8b455cf7c0bf0ba428
alex_Palladis <alehandro4443@gmail.com>
2024-12-23 23:31:37 +0200

    cookies

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,10 +2,14 @@ import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
+  console.log('Cookies:', req.cookies); // Log cookies for debugging
   const token = req.cookies.access_token;
   console.log('Token:', token); // Debug log
 
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
+  if (!token) {
+    console.log('No token in cookies:', req.cookies); 
+    return next(errorHandler(401, 'Unauthorized'));
+  }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
