ead92547c41cffd04fb3343f1088e442a845eb01
alex_Palladis <alehandro4443@gmail.com>
2024-12-23 21:08:55 +0200

    changes

--- api/index.js
+++ api/index.js
@@ -26,8 +26,8 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'PATCH' ,'DELETE'],
   }));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!')
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 });
 
 app.use('/api/user',userRouter);
