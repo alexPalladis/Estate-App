56df51ba3679ea06b1bd3ae4d612965493f73b22
alex_Palladis <alehandro4443@gmail.com>
2024-12-23 23:40:30 +0200

    parser

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,7 +27,7 @@ export const signin = async (req,res,next) => {
         const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
         const {password: pass,...rest} = validUser._doc;
         res
-        .cookie('access_token',token,{httpOnly: true})
+        .cookie('access_token',token,{httpOnly: true , secure:true})
         .status(200)
         .json(validUser);
     } catch (error) {
@@ -42,7 +42,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie('access_token', token, { httpOnly: true, secure:true })
         .status(200)
         .json(rest);
     } else {
